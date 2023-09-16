b38e56888dc3696d5c936d6ba0d152b010d17e0a
MaxikForever <69801190+MaxikForever@users.noreply.github.com>
2023-09-16 17:27:13 +0300

    full working slider with dots and auto scroll styled text and buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 'use strict'
 import { products } from "./api.js";
+import * as slider from "./slider.js"
+
 const productsList = products; 
 
 const productsDiv = document.querySelector('.pro-container[attribute="api"]');
