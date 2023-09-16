2409bb0f3656bef24ce5558e21481e4de1c35c5a
MaxikForever <69801190+MaxikForever@users.noreply.github.com>
2023-09-16 15:41:22 +0300

    integrated the api data to the page and styled it

--- scripts/main.js
+++ scripts/main.js
@@ -2,40 +2,27 @@
 import { products } from "./api.js";
 const productsList = products; 
 
-console.log(productsList)
-// call every element from productList
-console.log(productsList.length)
-const productsDiv = document.getElementById("products");
-for (let i=0 ;i<productsList.length; ++i)  // for each item in the array, do this:  
-{
+const productsDiv = document.querySelector('.pro-container[attribute="api"]');
+
+
+
+productsList.forEach(prd =>{
+    if(prd.category == 'electronics' || prd.category == `jewelery`){
+        return
+    }
+    const stars = Array(Math.round(prd.rating.rate)).fill('<i class="fas fa-star"></i>').join('');
     productsDiv.innerHTML += `
-    <p>a</p>
-    <div class="product">
-        <img src="${productsList[i].image}" alt="${productsList[i].title}">
-        <h3>${productsList[i].title}</h3>
-        <p>${productsList[i].description}</p>
-        <p>${productsList[i].price}</p>
+    <div class="pro">
+        <img src="${prd.image}" alt="${prd.title}">
+      <div class="des">
+      <span>${prd.category}</span>
+      <h5>${prd.title}</h5>
+      <div class="start"> 
+        ${stars}
+      </div>
+      <h4>${prd.price}$</h4>
+      </div>
+      <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
     </div>
     `
-}
-
-
-// const products = [];
-
-// async function getApiData() {
-//     try {
-//         const response = await fetch('https://fakestoreapi.com/products');
-//         const data = await response.json();
-
-//         data.forEach(product => {
-//             products.push(product);
-//         });
-
-
-//     } catch (error) {
-//         console.log("error", error);
-//     }
-// }
-
-// await getApiData();
-// export { products };
+})
